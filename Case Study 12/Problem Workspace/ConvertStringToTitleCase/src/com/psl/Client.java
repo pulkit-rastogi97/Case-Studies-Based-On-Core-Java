26833527b4329e9f69a8abf1e51d647e64fecfcc
Pulkit Rastogi <pulkit.rastogi97@gmail.com>
2020-03-28 00:55:43 +0530

    CS12 Created : Problem Workspace added

---
+++ Case Study 12/Problem Workspace/ConvertStringToTitleCase/src/com/psl/Client.java
@@ -0,0 +1,15 @@
+package com.psl;
+
+public class Client {
+
+	public String convertToTitle(String string){
+	
+		
+        
+	}
+		public static void main(String[] args) {
+				Client client = new Client();
+				client.convertToTitle(null);
+	
+		}
+}
