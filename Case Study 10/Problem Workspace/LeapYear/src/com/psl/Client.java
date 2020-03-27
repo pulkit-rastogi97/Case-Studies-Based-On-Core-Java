0bdd6cea5fcd02443d9970a86d9d908ab5a03f6b
Pulkit Rastogi <pulkit.rastogi97@gmail.com>
2020-03-28 00:55:31 +0530

    CS10 Created : Problem Workspace added

---
+++ Case Study 10/Problem Workspace/LeapYear/src/com/psl/Client.java
@@ -0,0 +1,14 @@
+package com.psl;
+
+public class Client {
+
+	public boolean isLeapYear(int year) {
+		
+	}
+
+	public static void main(String[] args) {
+		Client client = new Client();
+		client.isLeapYear(2020);
+	}
+
+}
